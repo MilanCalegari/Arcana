803c8285c117ae762866ad067fd4046a866f753b
MilanCalegari <milan.calegari@unesp.br>
2025-01-01 12:40:02 -0300

    fix: all paths

--- scripts/get_cards.py
+++ scripts/get_cards.py
@@ -7,22 +7,23 @@ import requests
 
 def get_cards():
     # Create data directory
-    os.makedirs('home/user/app/data', exist_ok=True)
-    
+    os.makedirs("/home/user/app/data", exist_ok=True)
+
     # Download zip file
-    url = 'https://www.kaggle.com/api/v1/datasets/download/lsind18/tarot-json'
-    zip_file = '/home/user/app/tarot-json.zip'
-    
+    url = "https://www.kaggle.com/api/v1/datasets/download/lsind18/tarot-json"
+    zip_file = "/home/user/app/tarot-json.zip"
+
     response = requests.get(url)
-    with open(zip_file, 'wb') as f:
+    with open(zip_file, "wb") as f:
         f.write(response.content)
-    
+
     # Extract contents
-    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-        zip_ref.extractall('home/user/app/data')
-    
+    with zipfile.ZipFile(zip_file, "r") as zip_ref:
+        zip_ref.extractall("home/user/app/data")
+
     # Remove zip file
     os.remove(zip_file)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     get_cards()
